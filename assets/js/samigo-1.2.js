43e9a9bded8de8f12835406561a813da1ec90102
Alok mishra <37037009+Alokmishra3751@users.noreply.github.com>
2022-06-20 09:41:54 +0530

    scrollbar fixed

--- assets/js/samigo-1.2.js
+++ assets/js/samigo-1.2.js
@@ -32,10 +32,10 @@ const effect = VANTA.NET({
   minHeight: 200.00,
   minWidth: 200.00,
   scale: 1.00,
-  scaleMobile: 1.7,
-  points: 15.00,
-  maxDistance: 20.00,
-  spacing: 14.00
+  scaleMobile: 1.00,
+  points: 8.00,
+  maxDistance: 24.00,
+  spacing: 17.00
 });
 
 function myFunction(x) {
@@ -58,7 +58,7 @@ x.addListener(myFunction) // Attach listener function on state changes
   scrollTrigger: {
   trigger: ".bg-effect",
   start:'center center',
-  scrub: 1.5,
+  scrub: 2.5,
   pin: true
   }
   })
