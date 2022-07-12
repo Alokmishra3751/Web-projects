989fd9b2e33bae7c314097218a42ca30f641037f
Alok mishra <37037009+Alokmishra3751@users.noreply.github.com>
2022-07-13 03:47:54 +0530

    Social token frontend

---
+++ Social-token/assets/js/main.js
@@ -0,0 +1,20 @@
+$(function(){
+
+ // $(window).on('load',function(){
+ //  setTimeout(function(){ // allowing 3 secs to fade out loader
+ //  $('.page-loader').fadeOut('fast');
+ //  $('.body-wrap').addClass("show");
+ //  },1800);
+ // });
+
+   AOS.init({
+  useClassNames: true,
+  initClassName: false,
+  animatedClassName: 'animate__animated',
+    });
+
+
+});
+
+
+
