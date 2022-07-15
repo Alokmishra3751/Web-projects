924f765d18eb716ba6fed70cf497f209bb30e5a1
Alok mishra <37037009+Alokmishra3751@users.noreply.github.com>
2022-07-16 00:57:19 +0530

    Social token update

--- Social-token/assets/js/main.js
+++ Social-token/assets/js/main.js
@@ -1,20 +1,25 @@
 $(function(){
 
- // $(window).on('load',function(){
- //  setTimeout(function(){ // allowing 3 secs to fade out loader
- //  $('.page-loader').fadeOut('fast');
- //  $('.body-wrap').addClass("show");
- //  },1800);
- // });
-
    AOS.init({
   useClassNames: true,
   initClassName: false,
+  once:true,
   animatedClassName: 'animate__animated',
     });
+  
+  $('.section-title'). attr('data-aos', 'animate__fadeIn'); 
 
+    
+
+    
+
+
+
+
+  AOS.refreshHard();
 
 });
+
 
 
 
