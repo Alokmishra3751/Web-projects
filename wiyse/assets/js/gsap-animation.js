4c5b748317257dd0ba25bff693b2967b3bac1396
Alok mishra <37037009+Alokmishra3751@users.noreply.github.com>
2022-12-13 02:38:12 +0530

    updated landing page 1

--- wiyse/assets/js/gsap-animation.js
+++ wiyse/assets/js/gsap-animation.js
@@ -2,6 +2,18 @@
     "use strict";
 
     $(document).on('ready', function() {
+    
+    $(window).on('scroll',function(){
+        if($(this).scrollTop() > 20 ){
+            $('.navbar').addClass('scrollUp');
+        }
+        else{
+            $('.navbar').removeClass('scrollUp');
+        }
+
+    });   
+
+
         gsap.registerPlugin(ScrollTrigger);
 
     //for collapsible header issue on mobile - don't  want it to refresh() on resize
@@ -15,16 +27,20 @@
         scrollTrigger: {
         trigger: ".intro",    
         start: "top top",
-        end:'bottom center',
+        end:"bottom top",
+        markers:true,
         pin: true,
         scrub:1,
         }
 
-        }).from('.intro p',{scale:3 , duration:3})
-        .to('.intro p',{ top:0 , left:'20%' , duration:2},'-=3')
-        .from('.intro h1',{opacity:0 , duration:.5})
-        .from('.intro h1',{scaleX:4 , scaleY:4  ,duration:3} , "-=.4")
-        .from('.intro h6',{opacity:0 , y:30 , stagger:.5});
+        }).fromTo('.intro p',{scale:4},{scale:1, top:0 , left:'20%',duration:15})
+        // .to('.intro p',{ top:0 , left:'20%' , duration:5},'-=5')
+        .from('.intro h1',{opacity:0,scaleX:6 , scaleY:6  ,duration:20})
+        .from('.intro h6',{opacity:0 , y:30 , stagger:3 , duration:20})
+        .to('.intro-wrapper',{scale:0.5 , transformOrigin:'left left', duration:15})
+        .from('.intro-para',{y:40 , opacity:0 , stagger:20 , duration:10})
+        .to('.intro-para',{y:-40 , duration:15});
+
 
 
     }); // end document ready function
