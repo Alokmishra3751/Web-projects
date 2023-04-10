fcc1c81e582936a8b8c6048f15f64e5813347928
Alok mishra <37037009+Alokmishra3751@users.noreply.github.com>
2023-04-10 19:33:58 +0530

    updated jpe-ii

--- JPE-Group/jpe-ii/assets/js/main.js
+++ JPE-Group/jpe-ii/assets/js/main.js
@@ -80,6 +80,41 @@ document.addEventListener('DOMContentLoaded', () => {
 
   });
 
+  // toggle search bar 
+
+  $("#toggleSearch").click(function() {
+    $(".search-container").toggleClass("show-search");
+  });
+
+  // Client-slider
+
+  $(".client-slick").slick({
+    speed: 5000,
+    autoplay: true,
+    autoplaySpeed: 0,
+    cssEase: 'linear',
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    infinite: true,
+    swipeToSlide: true,
+    focusOnSelect: true,
+    responsive: [
+            {
+              breakpoint: 750,
+              settings: {
+                slidesToShow: 3,
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 2,
+              }
+            }
+            ]
+});
+
+
   /**
    * Toggle mobile nav dropdowns
    */
